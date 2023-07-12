c3145b2ea0a8c399d008cc84528f37c1ee03ee22
hidayat <dayat.hidayat.rivai@gmail.com>
2023-07-12 15:06:23 +0700

    more OOP

--- src/main/kotlin/OOP/Function.kt
+++ src/main/kotlin/OOP/Function.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>) {
     sayName("Beast")
     var my_age = getMyAge(1995)
     println("I am $my_age years old")
+    println(inlineMethod())
 }
 
 fun getMyAge(year: Int): Int {
@@ -18,3 +19,5 @@ fun sayName(name: String) {
 fun welcome() {
     println("Welcome To My Application")
 }
+
+fun inlineMethod() = "This is an inline method"
---
+++ src/main/kotlin/OOP/InnerClass.kt
@@ -0,0 +1,17 @@
+package OOP
+
+class Outer() {
+    val myString = "this is String from Outer Class"
+
+    inner class InnerClass() {
+        fun getString() = "$myString and called from innerclass"
+    }
+}
+
+fun main() {
+    val outer = Outer()
+    println(outer.myString)
+
+    val innerClass = Outer().InnerClass()
+    println(innerClass.getString())
+}
